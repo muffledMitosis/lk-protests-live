3e3af4fa64d790dd32a82807c3c71c27b4aec099
Meth Munindradasa <methsndr@gmail.com>
2022-04-16 22:14:44 +0530

    Me complaining

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
 
 import L from "leaflet";
 
-// delete L.Icon.Default.prototype._getIconUrl;
+import { LatLngExpression } from "leaflet";
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
-  iconUrl: require("leaflet/dist/images/marker-icon.png").default,
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
-});
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
+    iconUrl: require("leaflet/dist/images/marker-icon.png").default,
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+  });
+// Some bug in leaflet that causes it to not load images. The react-leaflet
+// library is very poorly maintained
 
 function App() {
   const position: LatLngExpression = [48.864716, 2.349];
+
+
+  console.log("Lol");
 
   return (
     <div className="App">
