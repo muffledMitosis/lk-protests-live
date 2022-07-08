f99eaa4420fa775d9bdd51665c421576c1f3a6ca
Meth Munindradasa <methsndr@gmail.com>
2022-07-08 12:58:15 +0530

    backup

--- public/mapview/mapstuff.js
+++ public/mapview/mapstuff.js
@@ -1,32 +1,35 @@
-var map = L.map('map').setView({lon: 0, lat: 0}, 2);
+var map = L.map("map").setView({ lon: 0, lat: 0 }, 2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-      }).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+}).addTo(map);
 
 // show the scale bar on the lower left corner
-L.control.scale({imperial: true, metric: true}).addTo(map);
+L.control.scale({ imperial: true, metric: true }).addTo(map);
 
-const populateMap = async ()=>{
+const populateMap = async () => {
   let coordCount = 0;
-  const res = await fetch("http://192.168.1.169:3838/api/all", {
-    method: 'GET',
+  const res = await fetch("http://192.168.1.198:3838/api/all", {
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 
   const rawdata = await res.json();
 
-  for(const protest of rawdata){
+  for (const protest of rawdata) {
     coords = protest["loc"]["coordinates"];
-    if(coords.length == 2){
+    if (coords.length == 2) {
       //TODO: Check for proper coordinate format
       //Some of the coordinates have garbage input
-      console.log(protest);
-      coordCount = coordCount+1;
-      L.marker({lat: coords[0], lon: coords[1]}).bindPopup('Protest').addTo(map);
+      // console.log(protest);
+      coordCount = coordCount + 1;
+      L.marker({ lat: coords[0], lon: coords[1] })
+        .bindPopup("<h1>lol</h1>")
+        .addTo(map);
     }
   }
   console.log(coordCount);
